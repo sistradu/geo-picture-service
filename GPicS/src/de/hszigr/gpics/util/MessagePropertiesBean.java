2a882abce2f6ab9fece876a79ca9b33b39dcbde6
scholzrico@web.de <scholzrico@web.de@3ca12dde-09b1-40c5-0a3b-7456efe9e2eb>
2011-04-27 17:36:06 +0000

    Message.Properties auch für Beans

---
+++ GPicS/src/de/hszigr/gpics/util/MessagePropertiesBean.java
@@ -0,0 +1,25 @@
+package de.hszigr.gpics.util;
+
+import javax.faces.context.FacesContext;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rico Scholz
+ * Date: 27.04.11
+ * Time: 18:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class MessagePropertiesBean {
+
+	public String getPropertiesMessage(String key) {
+
+		FacesContext context = FacesContext.getCurrentInstance();
+
+		String text = Utils.getMessageResourceString(context.getApplication()
+                .getMessageBundle(), key, null, context.getViewRoot()
+                .getLocale());
+
+		return text;
+	}
+
+}
