efb8c0de3ede660cfe914d2e9fd7627af5c5c1f9
markus.ullrich.decopt@googlemail.com <markus.ullrich.decopt@googlemail.com@3ca12dde-09b1-40c5-0a3b-7456efe9e2eb>
2011-04-27 08:31:47 +0000

    Failed commit: Default

---
+++ trunk/GPicS/testsrc/de/hszigr/gpics/db/MockNutzerConnectorTest.java
@@ -0,0 +1,204 @@
+package de.hszigr.gpics.db;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Markus Ullrich
+ * Date: 27.04.11
+ * Time: 09:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class MockNutzerConnectorTest {
+
+    private MockNutzerConnector connector;
+
+    @Before
+    public void setUp() throws Exception {
+        connector = new MockNutzerConnector();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        connector = null;
+    }
+
+    @Test
+    public void testGetNutzerByID() throws Exception {
+        org.w3c.dom.Document doc = connector.getNutzerByID(0);
+        NodeList nameNodes = doc.getElementsByTagName("name");
+        NodeList passwordNodes = doc.getElementsByTagName("password");
+        NodeList emailNodes = doc.getElementsByTagName("email");
+        NodeList albumNodes = doc.getElementsByTagName("album");
+        Assert.assertEquals(1,nameNodes.getLength());
+        Assert.assertEquals(1,passwordNodes.getLength());
+        Assert.assertEquals(1,emailNodes.getLength());
+        Assert.assertEquals(2,albumNodes.getLength());
+        Assert.assertEquals("Karl", nameNodes.item(0).getTextContent());
+        Assert.assertEquals("karl@web.de", emailNodes.item(0).getTextContent());
+        String password = "test";
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            byte[] digest = md.digest(password.getBytes());
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < digest.length; i++) {
+                hexString.append(Integer.toHexString(0xFF & digest[i]));
+            }
+            password = hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(password, passwordNodes.item(0).getTextContent());
+        Assert.assertEquals("Görlitz", albumNodes.item(0).getAttributes().item(0).getTextContent());
+        Assert.assertEquals("Zittau", albumNodes.item(1).getAttributes().item(0).getTextContent());
+
+        doc = connector.getNutzerByID(1);
+        nameNodes = doc.getElementsByTagName("name");
+        passwordNodes = doc.getElementsByTagName("password");
+        emailNodes = doc.getElementsByTagName("email");
+        albumNodes = doc.getElementsByTagName("album");
+        Assert.assertEquals(1,nameNodes.getLength());
+        Assert.assertEquals(1,passwordNodes.getLength());
+        Assert.assertEquals(1,emailNodes.getLength());
+        Assert.assertEquals(1,albumNodes.getLength());
+        Assert.assertEquals("Günther", nameNodes.item(0).getTextContent());
+        Assert.assertEquals("guenther@web.de", emailNodes.item(0).getTextContent());
+        password = "password";
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            byte[] digest = md.digest(password.getBytes());
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < digest.length; i++) {
+                hexString.append(Integer.toHexString(0xFF & digest[i]));
+            }
+            password = hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(password, passwordNodes.item(0).getTextContent());
+        Assert.assertEquals("Oberlausitz", albumNodes.item(0).getAttributes().item(0).getTextContent());
+    }
+
+    @Test
+    public void testGetNutzerByName() throws Exception {
+        org.w3c.dom.Document doc = connector.getNutzerByName("Karl");
+        NodeList nameNodes = doc.getElementsByTagName("name");
+        NodeList passwordNodes = doc.getElementsByTagName("password");
+        NodeList emailNodes = doc.getElementsByTagName("email");
+        NodeList albumNodes = doc.getElementsByTagName("album");
+        Assert.assertEquals(1,nameNodes.getLength());
+        Assert.assertEquals(1,passwordNodes.getLength());
+        Assert.assertEquals(1,emailNodes.getLength());
+        Assert.assertEquals(2,albumNodes.getLength());
+        Assert.assertEquals("Karl", nameNodes.item(0).getTextContent());
+        Assert.assertEquals("karl@web.de", emailNodes.item(0).getTextContent());
+        String password = "test";
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            byte[] digest = md.digest(password.getBytes());
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < digest.length; i++) {
+                hexString.append(Integer.toHexString(0xFF & digest[i]));
+            }
+            password = hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(password, passwordNodes.item(0).getTextContent());
+        Assert.assertEquals("Görlitz", albumNodes.item(0).getAttributes().item(0).getTextContent());
+        Assert.assertEquals("Zittau", albumNodes.item(1).getAttributes().item(0).getTextContent());
+
+        doc = connector.getNutzerByName("Günther");
+        nameNodes = doc.getElementsByTagName("name");
+        passwordNodes = doc.getElementsByTagName("password");
+        emailNodes = doc.getElementsByTagName("email");
+        albumNodes = doc.getElementsByTagName("album");
+        Assert.assertEquals(1,nameNodes.getLength());
+        Assert.assertEquals(1,passwordNodes.getLength());
+        Assert.assertEquals(1,emailNodes.getLength());
+        Assert.assertEquals(1,albumNodes.getLength());
+        Assert.assertEquals("Günther", nameNodes.item(0).getTextContent());
+        Assert.assertEquals("guenther@web.de", emailNodes.item(0).getTextContent());
+        password = "password";
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            byte[] digest = md.digest(password.getBytes());
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < digest.length; i++) {
+                hexString.append(Integer.toHexString(0xFF & digest[i]));
+            }
+            password = hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(password, passwordNodes.item(0).getTextContent());
+        Assert.assertEquals("Oberlausitz", albumNodes.item(0).getAttributes().item(0).getTextContent());
+    }
+
+    @Test
+    public void testGetAllNutzer(){
+        org.w3c.dom.Document doc = connector.getAllNutzer();
+        NodeList nutzerNodes = doc.getElementsByTagName("benutzer");
+        Assert.assertEquals(2,nutzerNodes.getLength());
+        Element nutzerNode = (Element) nutzerNodes.item(0);
+        NodeList nameNodes = nutzerNode.getElementsByTagName("name");
+        NodeList passwordNodes = nutzerNode.getElementsByTagName("password");
+        NodeList emailNodes = nutzerNode.getElementsByTagName("email");
+        NodeList albumNodes = nutzerNode.getElementsByTagName("album");
+        Assert.assertEquals(1,nameNodes.getLength());
+        Assert.assertEquals(1,passwordNodes.getLength());
+        Assert.assertEquals(1,emailNodes.getLength());
+        Assert.assertEquals(2,albumNodes.getLength());
+        Assert.assertEquals("Karl", nameNodes.item(0).getTextContent());
+        Assert.assertEquals("karl@web.de", emailNodes.item(0).getTextContent());
+        String password = "test";
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            byte[] digest = md.digest(password.getBytes());
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < digest.length; i++) {
+                hexString.append(Integer.toHexString(0xFF & digest[i]));
+            }
+            password = hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(password, passwordNodes.item(0).getTextContent());
+        Assert.assertEquals("Görlitz", albumNodes.item(0).getAttributes().item(0).getTextContent());
+        Assert.assertEquals("Zittau", albumNodes.item(1).getAttributes().item(0).getTextContent());
+
+        nutzerNode = (Element) nutzerNodes.item(1);
+        nameNodes = nutzerNode.getElementsByTagName("name");
+        passwordNodes = nutzerNode.getElementsByTagName("password");
+        emailNodes = nutzerNode.getElementsByTagName("email");
+        albumNodes = nutzerNode.getElementsByTagName("album");
+        Assert.assertEquals(1,nameNodes.getLength());
+        Assert.assertEquals(1,passwordNodes.getLength());
+        Assert.assertEquals(1,emailNodes.getLength());
+        Assert.assertEquals(1,albumNodes.getLength());
+        Assert.assertEquals("Günther", nameNodes.item(0).getTextContent());
+        Assert.assertEquals("guenther@web.de", emailNodes.item(0).getTextContent());
+        password = "password";
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            byte[] digest = md.digest(password.getBytes());
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < digest.length; i++) {
+                hexString.append(Integer.toHexString(0xFF & digest[i]));
+            }
+            password = hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(password, passwordNodes.item(0).getTextContent());
+        Assert.assertEquals("Oberlausitz", albumNodes.item(0).getAttributes().item(0).getTextContent());
+    }
+}
