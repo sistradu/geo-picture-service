faf5678a3ad8bd6bd8306a65add168c79d58e24d
schicht.m@googlemail.com <schicht.m@googlemail.com@3ca12dde-09b1-40c5-0a3b-7456efe9e2eb>
2011-04-27 10:31:36 +0000

    Failed commit: Default

--- GPicS/src/de/hszigr/gpics/primefaces_beans/CalendarBean.java
+++ GPicS/src/de/hszigr/gpics/primefaces_beans/CalendarBean.java
@@ -8,11 +8,7 @@ public class CalendarBean {
   
     private Date date1;  
       
-    private Date date2;  
-      
-    private Date date3;  
-      
-    public Date getDate1() {  
+    public Date getDate1() {
         return date1;  
     }  
   
@@ -20,19 +16,5 @@ public class CalendarBean {
         this.date1 = date1;  
     }  
   
-    public Date getDate2() {  
-        return date2;  
-    }  
-  
-    public void setDate2(Date date2) {  
-        this.date2 = date2;  
-    }  
-      
-    public Date getDate3() {  
-        return date3;  
-    }  
-  
-    public void setDate3(Date date3) {  
-        this.date3 = date3;  
-    }  
+
 }  
