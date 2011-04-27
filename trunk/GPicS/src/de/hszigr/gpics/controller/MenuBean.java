aa8796fdec2c7c98492729b36f1689c163bac131
scholzrico@web.de <scholzrico@web.de@3ca12dde-09b1-40c5-0a3b-7456efe9e2eb>
2011-04-27 09:46:43 +0000

    Page uploaded

--- trunk/GPicS/src/de/hszigr/gpics/controller/MenuBean.java
+++ trunk/GPicS/src/de/hszigr/gpics/controller/MenuBean.java
@@ -5,6 +5,8 @@ import org.primefaces.component.submenu.Submenu;
 import org.primefaces.model.DefaultMenuModel;
 import org.primefaces.model.MenuModel;
 
+import javax.faces.bean.ManagedBean;
+
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,6 +15,7 @@ import org.primefaces.model.MenuModel;
  * Time: 11:07
  * To change this template use File | Settings | File Templates.
  */
+@ManagedBean
 public class MenuBean {
 
 	private MenuModel model;
@@ -36,7 +39,13 @@ public class MenuBean {
 	 	submenu.setLabel("öffentliche Alben");
 
 	 	item = new MenuItem();
-	 	item.setValue("Dynamic Menuitem 2.1");
+	 	item.setValue("dsgfs");
+	 	item.setUrl("#");
+	 	submenu.getChildren().add(item);
+
+
+        item = new MenuItem();
+	 	item.setValue("v,bgnbhnds");
 	 	item.setUrl("#");
 	 	submenu.getChildren().add(item);
 
@@ -46,5 +55,4 @@ public class MenuBean {
 	 public MenuModel getModel() {
 	 	 return model;
 	 }
-}
 }
