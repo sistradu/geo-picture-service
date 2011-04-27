5219b23e0a241ae15c026933427c3497f4f2d381
stefan.radusch <stefan.radusch@3ca12dde-09b1-40c5-0a3b-7456efe9e2eb>
2011-04-27 12:46:07 +0000

    metadata-extractor lib added, add ImageDataExtractor, Position, CoordinateCalculator, ImageDataExtractorTest, CoordinateCalculatorTest

---
+++ GPicS/src/de/hszigr/gpics/util/CoordinateCalculator.java
@@ -0,0 +1,28 @@
+package de.hszigr.gpics.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Stefan Radusch
+ * Date: 27.04.11
+ * Time: 13:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class CoordinateCalculator {
+
+    /**
+	 * @param position
+     * @param ref gibt an ob N, S, O, W
+	 * @return
+	 */
+	public String getDecimalCoordinate(String position, String ref) {
+		double degree = Double.parseDouble(position.substring(0, position.indexOf("\"")));
+		double minutes = Double.parseDouble(position.substring(position.indexOf("\"")+1, position.indexOf("'")));
+		double seconds = Double.parseDouble(position.substring(position.indexOf("'")+1));
+		degree += minutes/60;
+		degree += seconds/3600;
+        if(ref.equals("S")||ref.equals("W")){
+            degree *= -1;
+        }
+		return String.valueOf(degree);
+	}
+}
---
+++ GPicS/src/de/hszigr/gpics/util/Position.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package de.hszigr.gpics.util;
+
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
+/**
+ * @author Stefan Radusch
+ *
+ */
+public class Position {
+
+	private String latitudeRef;
+	private String latitude;
+//	private String latitudeDecimal;
+	private String longitudeRef;
+	private String longitude;
+//	private String longitudeDecimal;
+	private String altitude;
+	private String direction;
+    private GregorianCalendar timeStamp;
+	
+	public String getLatitudeRef() {
+		return latitudeRef;
+	}
+	public void setLatitudeRef(String latitudeRef) {
+		this.latitudeRef = latitudeRef;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitudeRef() {
+		return longitudeRef;
+	}
+	public void setLongitudeRef(String longitudeRef) {
+		this.longitudeRef = longitudeRef;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public void setAltitude(String altitude) {
+		this.altitude = altitude;
+	}
+	public String getAltitude() {
+		return altitude;
+	}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+	public String getDirection() {
+		return direction;
+	}
+
+    public GregorianCalendar getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(GregorianCalendar timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    @Override
+    public String toString(){
+        String string = super.toString();
+        if(this.getTimeStamp() != null){
+            string += "Time: " + new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(this.getTimeStamp().getTime());
+        }
+        return string;
+    }
+}
