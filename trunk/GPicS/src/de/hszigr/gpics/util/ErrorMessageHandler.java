b860c30b63c25b509081722979e39dad833bc367
stefan.radusch <stefan.radusch@3ca12dde-09b1-40c5-0a3b-7456efe9e2eb>
2011-04-27 20:25:30 +0000

    ErrorMessageHandler hinzugefügt. Außerdem in createUser, layout und sendPW messages eingebaut.

---
+++ trunk/GPicS/src/de/hszigr/gpics/util/ErrorMessageHandler.java
@@ -0,0 +1,19 @@
+package de.hszigr.gpics.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Stefan Radusch
+ * Date: 27.04.11
+ * Time: 21:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class ErrorMessageHandler {
+
+    public static void addErrorMessageToFacesMessage(String elementID, String errorMessage){
+        FacesMessage message = new FacesMessage(errorMessage);
+        FacesContext.getCurrentInstance().addMessage(elementID,  message);
+    }
+}
