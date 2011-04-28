a4c64576147bf1c201bf4eb94b412fbb1240356a
stefan.radusch <stefan.radusch@3ca12dde-09b1-40c5-0a3b-7456efe9e2eb>
2011-04-28 19:34:16 +0000

    PasswortUtil, createAlbum.xhtml, CreateEditAlbumController erstellt

---
+++ trunk/GPicS/src/de/hszigr/gpics/controller/CreateEditAlbumController.java
@@ -0,0 +1,55 @@
+package de.hszigr.gpics.controller;
+
+import de.hszigr.gpics.util.PasswortUtil;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Stefan Radusch
+ * Date: 28.04.11
+ * Time: 20:24
+ * To change this template use File | Settings | File Templates.
+ */
+@ManagedBean
+public class CreateEditAlbumController {
+
+    private int albumID;
+    private String albumName;
+    private String albumBeschreibung;
+    private String passwort;
+
+    public void generatePasswort(){
+        PasswortUtil util = new PasswortUtil();
+        String randomPasswort = util.erzeugeZufallsPasswort();
+        this.passwort = util.encryptWithMD5(randomPasswort);
+    }
+
+    public int getAlbumID() {
+        return albumID;
+    }
+
+    public void setAlbumID(int albumID) {
+        this.albumID = albumID;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getAlbumBeschreibung() {
+        return albumBeschreibung;
+    }
+
+    public void setAlbumBeschreibung(String albumBeschreibung) {
+        this.albumBeschreibung = albumBeschreibung;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+}
