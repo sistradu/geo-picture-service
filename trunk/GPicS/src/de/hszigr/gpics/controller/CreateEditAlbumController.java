084f602ed787112b29af473f4fd6ce796423bed4
stefan.radusch <stefan.radusch@3ca12dde-09b1-40c5-0a3b-7456efe9e2eb>
2011-05-05 16:31:45 +0000

    Bild, message.properties, UserController, CreateEditAlbumController, layout.xhtml, createAlbum.xhtml angepasst. PasswortUtilTest hinzugefügt. !!! In web.xml einträge für fileupload hinzugefügt. !!!

--- trunk/GPicS/src/de/hszigr/gpics/controller/CreateEditAlbumController.java
+++ trunk/GPicS/src/de/hszigr/gpics/controller/CreateEditAlbumController.java
@@ -1,8 +1,25 @@
 package de.hszigr.gpics.controller;
 
-import de.hszigr.gpics.util.PasswortUtil;
+import com.drew.imaging.jpeg.JpegProcessingException;
+import com.drew.metadata.MetadataException;
+import de.hszigr.gpics.db.MockAlbumConnector;
+import de.hszigr.gpics.db.MockBildConnector;
+import de.hszigr.gpics.db.interfaces.IAlbumConnector;
+import de.hszigr.gpics.db.interfaces.IBildConnector;
+import de.hszigr.gpics.util.*;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.io.*;
+import java.net.ConnectException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,7 +28,9 @@ import javax.faces.bean.ManagedBean;
  * Time: 20:24
  * To change this template use File | Settings | File Templates.
  */
+//TODO scope ändern
 @ManagedBean
+@SessionScoped
 public class CreateEditAlbumController {
 
     private int albumID;
@@ -19,7 +38,99 @@ public class CreateEditAlbumController {
     private String albumBeschreibung;
     private String passwort;
 
-    public void generatePasswort(){
+    private String uploadDir = "D:/upload/";
+    private List<Bild> bilder;
+
+    public CreateEditAlbumController() {
+        bilder = new ArrayList<Bild>();
+    }
+
+    public void handleFileUpload(FileUploadEvent event) {
+        UploadedFile file = event.getFile();
+
+        try {
+            FileOutputStream out = new FileOutputStream(uploadDir + file.getFileName());
+            out.write(file.getContents());
+            out.flush();
+            out.close();
+            updateBilder(uploadDir + file.getFileName());
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        FacesMessageHandler.createFacesMessageForID("uploader", file.getFileName() + " erfolgreich hochgeladen.");
+    }
+
+    private void updateBilder(String s) {
+        try {
+            Bild bild = new Bild();
+            bild.setName(s.substring(s.lastIndexOf("/") + 1));
+            bild.setPath(s);
+            InputStream stream = new FileInputStream(s);
+            StreamedContent image = new DefaultStreamedContent(stream, "image/jpeg", s.substring(s.lastIndexOf("/") + 1));
+            bild.setContent(image);
+            bild.setPublicBild(false);
+            bilder.add(bild);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public StreamedContent getImage() {
+        String name = FacesContext.getCurrentInstance()
+                .getExternalContext().getRequestParameterMap().get("name");
+        if(name!=null && !bilder.isEmpty()){
+            StreamedContent content = null;
+            for(Bild b : bilder){
+                if(b.getName().equals(name)){
+                    content = b.getContent();
+                }
+            }
+            return content;
+        }
+        return getDefaultContent();
+    }
+
+    private StreamedContent getDefaultContent(){
+        StreamedContent image = null;
+        try {
+            InputStream stream = new FileInputStream(uploadDir + "/gpics.jpg");
+            image = new DefaultStreamedContent(stream, "image/jpeg");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return image;
+    }
+
+    //todo album bearbeiten
+
+    public String speichern(){
+        try {
+        //TODO Connector ändern
+            IAlbumConnector connector = new MockAlbumConnector();
+            generatePasswort();
+            this.albumID = connector.createAlbum(albumName, passwort, albumBeschreibung);
+            speicherBilder();
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            FacesMessageHandler.createFacesMessageForID("saveAlbum", e.getMessage());
+        }
+        return "showAlbum";
+    }
+
+    private void speicherBilder() throws JpegProcessingException, MetadataException, ParseException, ConnectException {
+        for(Bild bild : bilder){
+            ImageDataExtractor extractor = new ImageDataExtractor();
+            Position pos = extractor.getPosition(bild.getPath());
+            CoordinateCalculator calculator = new CoordinateCalculator();
+            String longitudeDecimal = calculator.getDecimalCoordinate(pos.getLongitude(), pos.getLongitudeRef());
+            String latitudeDecimal = calculator.getDecimalCoordinate(pos.getLatitude(), pos.getLatitudeRef());
+            IBildConnector connector = new MockBildConnector();
+            connector.createBild(bild.getName(), bild.getBeschreibung(), bild.isPublicBild(), pos.getTimeStamp(), longitudeDecimal, latitudeDecimal, pos.getAltitude(), pos.getDirection());
+        }
+    }
+
+    private void generatePasswort() {
         PasswortUtil util = new PasswortUtil();
         String randomPasswort = util.erzeugeZufallsPasswort(4);
         this.passwort = util.encryptWithMD5(randomPasswort);
@@ -51,5 +162,17 @@ public class CreateEditAlbumController {
 
     public String getPasswort() {
         return passwort;
+    }
+
+    public List<Bild> getBilder() {
+        return bilder;
+    }
+
+    public void setBilder(List<Bild> bilder) {
+        this.bilder = bilder;
+    }
+
+    public boolean containsImages(){
+        return bilder.size()>0;
     }
 }
