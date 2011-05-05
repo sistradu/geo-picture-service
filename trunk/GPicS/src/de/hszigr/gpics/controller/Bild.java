f6d75988e0104e58a9ee01c79b72403e759a5cc4
stefan.radusch <stefan.radusch@3ca12dde-09b1-40c5-0a3b-7456efe9e2eb>
2011-05-05 12:43:14 +0000

    beim Bild einige Attribute hinzugefügt

--- trunk/GPicS/src/de/hszigr/gpics/controller/Bild.java
+++ trunk/GPicS/src/de/hszigr/gpics/controller/Bild.java
@@ -1,6 +1,11 @@
 package de.hszigr.gpics.controller;
 
+import com.sun.xml.internal.ws.developer.Stateful;
 import org.primefaces.model.StreamedContent;
+
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+import java.util.GregorianCalendar;
 
 /**
  * Created by IntelliJ IDEA.
@@ -9,9 +14,17 @@ import org.primefaces.model.StreamedContent;
  * Time: 13:31
  * To change this template use File | Settings | File Templates.
  */
-public class Bild {
+public class Bild implements Serializable{
 
+    private int bildID;
     private String name;
+    private String beschreibung;
+    private boolean isPublic;
+    private GregorianCalendar date;
+    private String longitude;
+    private String latitude;
+    private String altitude;
+    private String direction;
     private String path;
     private StreamedContent content;
 
@@ -37,5 +50,69 @@ public class Bild {
 
     public void setContent(StreamedContent content) {
         this.content = content;
+    }
+
+    public int getBildID() {
+        return bildID;
+    }
+
+    public void setBildID(int bildID) {
+        this.bildID = bildID;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public GregorianCalendar getDate() {
+        return date;
+    }
+
+    public void setDate(GregorianCalendar date) {
+        this.date = date;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(String altitude) {
+        this.altitude = altitude;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
     }
 }
