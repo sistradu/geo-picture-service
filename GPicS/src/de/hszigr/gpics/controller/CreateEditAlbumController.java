2ab13ed75aa6da38a87e32f1667d36282c801d4f
stefan.radusch <stefan.radusch@3ca12dde-09b1-40c5-0a3b-7456efe9e2eb>
2011-05-10 18:03:00 +0000

    CreateEditAlbumController: Methoden für Album bearbeiten hinzugefügt.

--- GPicS/src/de/hszigr/gpics/controller/CreateEditAlbumController.java
+++ GPicS/src/de/hszigr/gpics/controller/CreateEditAlbumController.java
@@ -12,15 +12,18 @@ import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 import org.primefaces.model.UploadedFile;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
 import java.io.*;
 import java.net.ConnectException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -41,7 +44,8 @@ public class CreateEditAlbumController {
     private String passwort;
 
     //TODO uploadDir
-    private String uploadDir = "/home/upload/";
+//    private String uploadDir = "/home/pics/";
+    private String uploadDir = "D:/upload/";
     private List<Bild> bilder;
 
     private int selectedBId;
@@ -58,14 +62,212 @@ public class CreateEditAlbumController {
             out.write(file.getContents());
             out.flush();
             out.close();
-            updateBilder(uploadDir + file.getFileName());
+            updateBilderListe(uploadDir + file.getFileName());
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
         FacesMessageHandler.createFacesMessageForID("uploader", file.getFileName() + " erfolgreich hochgeladen.");
     }
 
-    private void updateBilder(String s) {
+    public StreamedContent getImage() {
+        String name = FacesContext.getCurrentInstance()
+                .getExternalContext().getRequestParameterMap().get("name");
+        if (name != null && !bilder.isEmpty()) {
+            StreamedContent content = null;
+            for (Bild b : bilder) {
+                if (b.getName().equals(name)) {
+                    content = b.getContent();
+                }
+            }
+            return content;
+        }
+        return getDefaultContent();
+    }
+
+    //TODO einstieg für bearbeiten
+    public String ladeAlbum() {
+        String name = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("albumName");
+        try {
+            if (name != null) {
+                ladeAttributeAusDB(name);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            FacesMessageHandler.createFacesMessageForID("saveAlbum", e.getMessage());
+        }
+        return "createAlbum";
+    }
+
+    public void loescheBild() {
+        try {
+            int bildId = Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("bildID"));
+            for(int i = 0; i<bilder.size();i++){
+                Bild bild = bilder.get(i);
+                if(bildId == bild.getBildID()){
+                    File f = new File(bild.getPath());
+                    bilder.remove(bild);
+                    //TODO connector ändern
+                    IBildConnector connector = new MockBildConnector();
+                    connector.deleteBild(bild.getBildID());
+                }
+            }
+        } catch (ConnectException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            FacesMessageHandler.createFacesMessageForID("saveAlbum", e.getMessage());
+        }
+    }
+
+    //TODO aufruf zum speichern des bearbeiteten album
+    public String updateAlbum(){
+        try {
+            //TODO connector ändern
+            IAlbumConnector albumConnector = new MockAlbumConnector();
+            albumConnector.updateAlbum(this.albumID, this.albumName, this.passwort, this.albumBeschreibung);
+            for(Bild bild : bilder){
+                IBildConnector bildConnector = new MockBildConnector();
+                bildConnector.updateBild(bild.getBildID(), bild.getName(), bild.getBeschreibung(), bild.isPublicBild(), bild.getDate(), bild.getLongitude(), bild.getLatitude(), bild.getAltitude(), bild.getDirection());
+            }
+        } catch (ConnectException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            FacesMessageHandler.createFacesMessageForID("saveAlbum", e.getMessage());
+            return null;
+        }
+        return "showAlbum";
+    }
+
+    public String createAlbum() {
+        try {
+            if (this.albumName == null) {
+                MessagePropertiesBean msg = new MessagePropertiesBean();
+                FacesMessageHandler.createFacesMessageForID("createAlbumName", msg.getPropertiesMessage("forgotAlbumName"));
+                return null;
+
+            }
+            //TODO Connector ändern
+            IAlbumConnector connector = new MockAlbumConnector();
+            generatePasswort();
+            this.albumID = connector.createAlbum(albumName, passwort, albumBeschreibung);
+            Document album = connector.getAlbum(albumName);
+            createBilder(album);
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            FacesMessageHandler.createFacesMessageForID("saveAlbum", e.getMessage());
+        }
+        return "showAlbum";
+    }
+
+    public void checkboxSelectAction() {
+        for (Bild bild : bilder) {
+            if (selectedBId == bild.getBildID()) {
+                bild.setPublicBild(true);
+            }
+        }
+    }
+
+    public boolean isNewAlbum(){
+        return this.albumID == 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void ladeAttributeAusDB(String name) throws ConnectException {
+        //TODO Connector ändern
+        IAlbumConnector albumConnector = new MockAlbumConnector();
+        Document doc = albumConnector.getAlbum(name);
+        this.albumID = Integer.parseInt(doc.getElementsByTagName("id").item(0).getTextContent());
+        this.albumName = name;
+        this.passwort = doc.getElementsByTagName("passwort").item(0).getTextContent();
+        this.albumBeschreibung = doc.getElementsByTagName("description").item(0).getTextContent();
+
+        //TODO Connector ändern
+        IBildConnector bildConnector = new MockBildConnector();
+        this.bilder = new ArrayList<Bild>();
+        List<Node> nodeList = (List<Node>) doc.getElementsByTagName("bild");
+        for (Node node : nodeList) {
+            Bild bild = ladeBildAusDB(bildConnector, node);
+            bilder.add(bild);
+        }
+    }
+
+    private void createBilder(Document album) throws JpegProcessingException, MetadataException, ParseException, ConnectException, FileNotFoundException {
+        for (Bild bild : bilder) {
+            ImageDataExtractor extractor = new ImageDataExtractor();
+            Position pos = extractor.getPosition(bild.getPath());
+            CoordinateCalculator calculator = new CoordinateCalculator();
+            String longitudeDecimal = calculator.getDecimalCoordinate(pos.getLongitude(), pos.getLongitudeRef());
+            bild.setLongitude(longitudeDecimal);
+            String latitudeDecimal = calculator.getDecimalCoordinate(pos.getLatitude(), pos.getLatitudeRef());
+            bild.setLatitude(latitudeDecimal);
+            bild.setDate(pos.getTimeStamp());
+            IBildConnector connector = new MockBildConnector();
+//            bilder.indexOf(bild);
+//            connector.createBild(bild.getName(), bild.getBeschreibung(), bild.isPublicBild(), pos.getTimeStamp(), longitudeDecimal, latitudeDecimal, pos.getAltitude(), pos.getDirection());
+            connector.createBild(bild.getName(), bild.getBeschreibung(), bild.isPublicBild(), bild.getDate(), bild.getPath(), bild.getLongitude(), bild.getLatitude(), bild.getAltitude(), bild.getDirection(), album);
+        }
+    }
+
+    private Bild ladeBildAusDB(IBildConnector bildConnector, Node node) throws ConnectException {
+        String bildName = node.getAttributes().getNamedItem("id").getTextContent();
+        Document bildDoc = bildConnector.getBilderByName(bildName);
+        Bild bild = new Bild();
+        bild.setBildID(Integer.parseInt(getTextContentFromElement(bildDoc, "id")));
+        bild.setName(getTextContentFromElement(bildDoc, "name"));
+        bild.setBeschreibung(getTextContentFromElement(bildDoc, "description"));
+        String isPublic = getTextContentFromElement(bildDoc, "ispublic");
+        if (isPublic.equals("true")) {
+            bild.setPublicBild(true);
+        } else {
+            bild.setPublicBild(false);
+        }
+        String timeStamp = getTextContentFromElement(bildDoc, "timestamp");
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = sdf.parse(timeStamp);
+            GregorianCalendar cal = new GregorianCalendar();
+            cal.setTime(date);
+            bild.setDate(cal);
+        } catch (ParseException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        bild.setLongitude(getTextContentFromElement(bildDoc, "longitude"));
+        bild.setLatitude(getTextContentFromElement(bildDoc, "latitude"));
+        bild.setAltitude(getTextContentFromElement(bildDoc, "altitude"));
+        bild.setDirection(getTextContentFromElement(bildDoc, "direction"));
+        String filePosition = getTextContentFromElement(bildDoc, "fileposition");
+        bild.setPath(filePosition);
+        try {
+            InputStream stream = new FileInputStream(filePosition);
+            StreamedContent image = new DefaultStreamedContent(stream, "image/jpeg");
+            bild.setContent(image);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return bild;
+    }
+
+    private String getTextContentFromElement(Document bildDoc, String tagName) {
+        return bildDoc.getElementsByTagName(tagName).item(0).getTextContent();
+    }
+
+    private StreamedContent getDefaultContent() {
+        StreamedContent image = null;
+        try {
+            InputStream stream = new FileInputStream(uploadDir + "/gpics.jpg");
+            image = new DefaultStreamedContent(stream, "image/jpeg");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return image;
+    }
+
+    private void generatePasswort() {
+        PasswortUtil util = new PasswortUtil();
+        String randomPasswort = util.erzeugeZufallsPasswort(4);
+        this.passwort = util.encryptWithMD5(randomPasswort);
+    }
+
+    private void updateBilderListe(String s) {
         try {
             Bild bild = new Bild();
             bild.setName(s.substring(s.lastIndexOf("/") + 1));
@@ -81,77 +283,6 @@ public class CreateEditAlbumController {
         }
     }
 
-    public StreamedContent getImage() {
-        String name = FacesContext.getCurrentInstance()
-                .getExternalContext().getRequestParameterMap().get("name");
-        if(name!=null && !bilder.isEmpty()){
-            StreamedContent content = null;
-            for(Bild b : bilder){
-                if(b.getName().equals(name)){
-                    content = b.getContent();
-                }
-            }
-            return content;
-        }
-        return getDefaultContent();
-    }
-
-    private StreamedContent getDefaultContent(){
-        StreamedContent image = null;
-        try {
-            InputStream stream = new FileInputStream(uploadDir + "/gpics.jpg");
-            image = new DefaultStreamedContent(stream, "image/jpeg");
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-        return image;
-    }
-
-    //TODO album bearbeiten
-
-    public String speichern(){
-        try {
-        //TODO Connector ändern
-            IAlbumConnector connector = new MockAlbumConnector();
-            generatePasswort();
-            this.albumID = connector.createAlbum(albumName, passwort, albumBeschreibung);
-            Document album = connector.getAlbum(albumName);
-            speicherBilder(album);
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            FacesMessageHandler.createFacesMessageForID("saveAlbum", e.getMessage());
-        }
-        return "showAlbum";
-    }
-
-    private void speicherBilder(Document album) throws JpegProcessingException, MetadataException, ParseException, ConnectException, FileNotFoundException {
-        for(Bild bild : bilder){
-            ImageDataExtractor extractor = new ImageDataExtractor();
-            Position pos = extractor.getPosition(bild.getPath());
-            CoordinateCalculator calculator = new CoordinateCalculator();
-            String longitudeDecimal = calculator.getDecimalCoordinate(pos.getLongitude(), pos.getLongitudeRef());
-            String latitudeDecimal = calculator.getDecimalCoordinate(pos.getLatitude(), pos.getLatitudeRef());
-            IBildConnector connector = new MockBildConnector();
-            bilder.indexOf(bild);
-//            connector.createBild(bild.getName(), bild.getBeschreibung(), bild.isPublicBild(), pos.getTimeStamp(), longitudeDecimal, latitudeDecimal, pos.getAltitude(), pos.getDirection());
-            connector.createBild(bild.getName(), bild.getBeschreibung(), bild.isPublicBild(), pos.getTimeStamp(), bild.getPath(), longitudeDecimal, latitudeDecimal, bild.getAltitude(), bild.getDirection(), album);
-        }
-    }
-
-    private void generatePasswort() {
-        PasswortUtil util = new PasswortUtil();
-        String randomPasswort = util.erzeugeZufallsPasswort(4);
-        this.passwort = util.encryptWithMD5(randomPasswort);
-    }
-
-    public void checkboxSelectAction(){
-        for(Bild bild : bilder){
-            if(selectedBId==bild.getBildID()){
-                bild.setPublicBild(true);
-            }
-        }
-    }
 
     public int getAlbumID() {
         return albumID;
@@ -189,8 +320,8 @@ public class CreateEditAlbumController {
         this.bilder = bilder;
     }
 
-    public boolean containsImages(){
-        return bilder.size()>0;
+    public boolean containsImages() {
+        return bilder.size() > 0;
     }
 
     public int getSelectedBId() {
