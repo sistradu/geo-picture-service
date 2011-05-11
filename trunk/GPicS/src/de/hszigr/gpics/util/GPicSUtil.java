a834b66bd0048410460cbe03352d9bb9c002a403
stefan.radusch <stefan.radusch@3ca12dde-09b1-40c5-0a3b-7456efe9e2eb>
2011-05-11 11:54:31 +0000

    ImageUtil in GPicSUtil umbenannt. Methode createFacesMessageForID in GPicSUtil verschoben.

--- trunk/GPicS/src/de/hszigr/gpics/util/GPicSUtil.java
+++ trunk/GPicS/src/de/hszigr/gpics/util/GPicSUtil.java
@@ -3,6 +3,8 @@ package de.hszigr.gpics.util;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,12 +16,21 @@ import java.io.InputStream;
  * Time: 10:49
  * To change this template use File | Settings | File Templates.
  */
-public class ImageUtil {
+public class GPicSUtil {
 
     public static StreamedContent getStreamContent(String path) throws IOException {
         InputStream stream = new FileInputStream(path);
         StreamedContent image = new DefaultStreamedContent(stream, "image/jpeg");
-//        stream.close();
         return image;
+    }
+
+    /**
+     * generiert eine FacesMessage, die beim h:message-Element mit der id <elementID> angezeigt wird
+     * @param elementID
+     * @param errorMessage
+     */
+    public static void createFacesMessageForID(String elementID, String errorMessage){
+        FacesMessage message = new FacesMessage(errorMessage);
+        FacesContext.getCurrentInstance().addMessage(elementID,  message);
     }
 }
